d059984146127681d9ed3981aaabcfe5a1804274
ZHangPeng1999 <2074809357@qq.com>
2020-03-10 14:57:29 +0800

    springbootDesign init

---
+++ src/main/java/com/example/springbootdesign/SpringbootDesignApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootdesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDesignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDesignApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootdesign/SpringbootDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootdesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDesignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
