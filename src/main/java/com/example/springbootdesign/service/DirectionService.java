5db662c1c2fb6d8b48b4947e0a2d8a1ca1edb978
ZHangPeng1999 <2074809357@qq.com>
2020-03-22 17:06:48 +0800

    springbootDesign service01

--- src/main/java/com/example/springbootdesign/service/DirectionService.java
+++ src/main/java/com/example/springbootdesign/service/DirectionService.java
@@ -1,7 +1,11 @@
 package com.example.springbootdesign.service;
 
 import com.example.springbootdesign.entity.Direction;
+import com.example.springbootdesign.entity.DirectionElective;
+import com.example.springbootdesign.entity.Student;
+import com.example.springbootdesign.repository.DirectionElectiveRepository;
 import com.example.springbootdesign.repository.DirectionRepository;
+import com.example.springbootdesign.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,7 +19,10 @@ import java.util.List;
 public class DirectionService {
     @Autowired
     private DirectionRepository directionRepository;
-
+    @Autowired
+    private StudentRepository studentRepository;
+    @Autowired
+    private DirectionElectiveRepository directionElectiveRepository;
     /**
      * 添加方向
      * @param name
@@ -51,5 +58,13 @@ public class DirectionService {
      */
     private List<Direction> list(Integer id){
        return directionRepository.list(id);
+    }
+    private void update(Integer studentId,Integer directionId){
+        DirectionElective directionElective = new DirectionElective();
+        Direction direction=directionRepository.findById(directionId).orElse(null);
+        Student student= studentRepository.findById(studentId).orElse(null);
+        directionElective.setDirection(direction);
+        directionElective.setStudent(student);
+        directionElectiveRepository.save(directionElective);
     }
 }
